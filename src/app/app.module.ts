67824a38cc2bf97d97e7392c14104bb9fdd0f20a
gnarfugh <kmorales076@gmail.com>
2018-03-27 11:37:46 -0500

    adding new http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
+import {HttpModule} from "@angular/http";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from './app.component';
 import { EntryListComponent, EntryComponent } from './entries';
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule, HttpModule],
     declarations: [
         AppComponent, 
         EntryComponent,
